0bf460a9d541b61c824f0efb3405161434be21f4
Oliver Leppänen <oliverleppanen@gmail.com>
2018-10-25 23:54:43 +0300

    Fixed a path related problem in the bash script

--- hubble-wp.sh
+++ hubble-wp.sh
@@ -2,4 +2,4 @@
 PID=$(pgrep gnome-session | tail -n1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-/usr/bin/python /home/oliver/hubble-wp/hubble_wp.py
+/usr/bin/python $PWD/hubble_wp.py
